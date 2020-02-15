259b74ca5802eada0cb1365a81b9cce029821996
Ola <olawunmi263@yahoo.com>
2020-02-15 04:35:51 +0000

    I opened HF homepage!!!

--- src/test/java/co/uk/hf/stepDefinitions/ViewAboutUsPage.java
+++ src/test/java/co/uk/hf/stepDefinitions/ViewAboutUsPage.java
@@ -3,11 +3,19 @@ package co.uk.hf.stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ViewAboutUsPage
-{@Given("I navigate to h-f.co.uk")
-public void i_navigate_to_h_f_co_uk() {
+{
+    WebDriver driver;
 
+    @Given("I navigate to h-f.co.uk")
+public void i_navigate_to_h_f_co_uk() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.navigate().to("https://www.h-f.co.uk/");
 }
 
     @When("I click on the {string} tab")
